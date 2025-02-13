a024aaf744aaef543f3f817f805e18a658141279
JosefCervenka <pepakonto@gmail.com>
2025-02-13 20:04:01 +0100

    validators

---
+++ WebApp.Application/Utils/Validators/PasswordValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApp.Application.Utils.Validators.Base;
+
+namespace WebApp.Application.Utils.Validators
+{
+    public class PasswordValidator : BaseValidator<string>
+    {
+        public override bool IsValid(string value, out string validationErrorMessage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
