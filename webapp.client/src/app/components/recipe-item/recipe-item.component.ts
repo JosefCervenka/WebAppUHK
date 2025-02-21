58c4245276a977aef1b82f1f5699604b93f43560
JosefCervenka <pepakonto@gmail.com>
2025-02-21 16:32:30 +0100

    changes

--- webapp.client/src/app/components/recipe-item/recipe-item.component.ts
+++ webapp.client/src/app/components/recipe-item/recipe-item.component.ts
@@ -1,17 +1,26 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, ViewChild} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Recipe} from "../../models/Recipe";
 
 @Component({
   selector: 'app-recepie-item',
   templateUrl: './recipe-item.component.html',
-  styleUrl: './recipe-item.component.css'
+  styleUrl: './recipe-item.component.scss'
 })
 export class RecipeItemComponent {
 
   @Input() recipe?: Recipe;
 
-  constructor(private http: HttpClient) {
+  imageUrl?: string;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    if (this.recipe?.headerPhoto?.url) { // More robust check
+      this.http.get(this.recipe.headerPhoto.url, { responseType: 'blob' }).subscribe(imageFile => {
+        console.log(imageFile);
+        this.imageUrl = URL.createObjectURL(imageFile);
+      });
+    }
   }
 }
