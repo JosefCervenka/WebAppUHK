73b1ebdd54a0e11bb0bc49634eb69af0aaafc7a7
Josef Cervenka <cervejo2@uhk.cz>
2025-02-25 11:16:30 +0100

    changes

--- webapp.client/src/app/pages/registration/registration.component.ts
+++ webapp.client/src/app/pages/registration/registration.component.ts
@@ -37,7 +37,6 @@ export class RegistrationComponent {
       passwordAgain: this.passwordFormConfirmControl.value,
       email: this.emailFormControl.value,
       name: this.nameFormControl.value,
-
     }
 
     var login = {
